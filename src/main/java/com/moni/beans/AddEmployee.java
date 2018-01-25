871c83f0126bafab7633cd040d738118ff58ea40
MohanrajRamalingam <mohanrajram@yahoo.co.in>
2018-01-25 00:27:11 -0600

    somemore changes

--- src/main/java/com/moni/beans/AddEmployee.java
+++ src/main/java/com/moni/beans/AddEmployee.java
@@ -1,30 +1,20 @@
 package com.moni.beans;
 
-import static org.testng.Assert.assertEquals;
-
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.testng.annotations.Test;
 
 public class AddEmployee {
 
-	@Test
-	public void testEmployee() {
+	public static void main(String args[]) {
 		String confFile = "applicationContext.xml";
 
 		@SuppressWarnings("resource")
 		ApplicationContext context = new ClassPathXmlApplicationContext(confFile);
-		Employee empBean = (Employee) context.getBean("myBean2");
-		
+
+		Employee empBean = (Employee) context.getBean("");
 		empBean.setEmpId(12);
 		empBean.setEmpName("MR");
-		
 		System.out.println(empBean.getEmpId());
 		System.out.println(empBean.getEmpName());
-		
-		assertEquals(empBean.getEmpId(), 12, "Emp id not matching");
-		assertEquals(empBean.getEmpName(), "MR", "Emp name not maching");
-		
 	}
-
 }
--- src/main/java/com/moni/beans/AppConfig.java
+++ src/main/java/com/moni/beans/AppConfig.java
@@ -5,11 +5,11 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AppConfig {
-
-	@Bean(name="myBean1")
-	public SpringFirstTest getHelloWorld() {
-		return new SpringFirstTest();
-	}
+//
+//	@Bean(name="myBean1")
+//	public SpringFirstTest getHelloWorld() {
+//		return new SpringFirstTest();
+//	}
 	
 	@Bean(name="myBean2")
 	public Employee getEmployee() {
